802773289fd46ba0c80d3f424fb2b3244bd628cb
Roi S <roi.santos.mateos@gmail.com>
2025-01-19 11:02:38 +0100

    fix roiscript

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J 5bBigK_fives        # Nombre del trabajo
-#SBATCH -o 5bBigK_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e 5bBigK_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J 1worker_fives        # Nombre del trabajo
+#SBATCH -o 1w_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e 1w_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
-#SBATCH --mem=247G                # Memoria total
+#SBATCH --mem=16G                # Memoria total
 #SBATCH -p medium
 #SBATCH -t 3-00:00:00              # Tiempo máximo de ejecución (2 horas)
 # Cargar módulos necesarios
@@ -13,7 +13,6 @@ module load python/3.9.9
 
 
 cd /home/usc/ec/rsm/tfg_codebase_cesga/
-source ../venv/bin/activate
+source ../vroi/bin/activate
 # Ejecutar el script de entrenamiento
-srun python3 ./code/training/run_benchmark.py
-#srun --gres=gpu:a100:4 -c 48 --mem=48G --time=47:59:00 --pty
+srun python ./code/training/run_benchmark.py
