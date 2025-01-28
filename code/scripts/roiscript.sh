5e8dd04ca3753cb2c89e140cadfc1da2b15c8819
Roi S <roi.santos.mateos@gmail.com>
2025-01-28 11:11:52 +0100

    error con REC

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J 3chFR2_fives        # Nombre del trabajo
-#SBATCH -o 3chFR2_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e 3chFR2_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J 3chREC_fives        # Nombre del trabajo
+#SBATCH -o 3chREC_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e 3chREC_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=16G                # Memoria total
@@ -12,7 +12,7 @@ module load cesga/2020
 module load python/3.9.9
 
 
-cd /home/usc/ec/rsm/tfg_codebase_cesga/
+cd /home/usc/ec/rsm/fivesegmentor/
 source ../vroi/bin/activate
 # Ejecutar el script de entrenamiento
 srun python ./code/training/run_benchmark.py
