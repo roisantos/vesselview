1f4b0a80e256639100d1b1160c5fed427b9b0eac
roisantos <62201385+roisantos@users.noreply.github.com>
2025-01-30 13:03:21 +0100

    Update roiscript.sh

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
-# Define the model name (Change this variable to update everything)
-MODEL_NAME="RoiNetx1.5"
-
-#SBATCH -J ${MODEL_NAME}        # Job name dynamically set
-#SBATCH -o ${MODEL_NAME}_output_%j.log   # Output log file
-#SBATCH -e ${MODEL_NAME}_error_%j.log    # Error log file
-#SBATCH --gres=gpu:a100:1        # Request 1 GPU A100
-#SBATCH -c 32                    # 32 CPU cores
-#SBATCH --mem=16G                # Total memory
+#SBATCH -J RoiNetx1.5        # Nombre del trabajo
+#SBATCH -o RoiNetx1.5_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e RoiNetx1.5_error_%j.log    # Archivo para la salida de errores
+#SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
+#SBATCH -c 32                    # 32 núcleos de CPU
+#SBATCH --mem=16G                # Memoria total
 #SBATCH -p medium
-#SBATCH -t 3-00:00:00            # Max execution time (3 days)
-
-# Load necessary modules
+#SBATCH -t 3-00:00:00              # Tiempo máximo de ejecución (2 horas)
+# Cargar módulos necesarios
 module load cesga/2020
 module load python/3.9.9
 
+
 cd /home/usc/ec/rsm/fivesegmentor/
 source ../vroi/bin/activate
-
-# Execute training script with the model
-srun python ./code/training/run_benchmark.py -model "${MODEL_NAME}"
+# Ejecutar el script de entrenamiento
+srun python ./code/training/run_benchmark.py -model "RoiNetx1.5"
