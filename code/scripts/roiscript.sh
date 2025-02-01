93680a648c14178094656da53012170e15a507a9
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-01 06:40:57 +0100

    batch size

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J RoiNet        # Nombre del trabajo
-#SBATCH -o RoiNet_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e RoiNet_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J RoiBS2        # Nombre del trabajo
+#SBATCH -o RoiBS2_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e RoiBS2_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
-#SBATCH --mem=16G                # Memoria total
+#SBATCH --mem=64G                # Memoria total
 #SBATCH -p medium
 #SBATCH -t 3-00:00:00              # Tiempo máximo de ejecución (2 horas)
 # Cargar módulos necesarios
