207b84a170977193b2e3fad198c478aadc9ed6f8
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-11 16:09:36 +0100

    vintage

--- code/models/frnet.py
+++ code/models/frnet.py
@@ -17,7 +17,7 @@ sys.path.insert(0, ROOT_DIR)
 from models.common import *
 
 class FRNet(nn.Module):
-    def __init__(self, ch_in, ch_out, ls_mid_ch=([32]*6), out_k_size=15, k_size=7,
+    def __init__(self, ch_in, ch_out, ls_mid_ch=([32]*5), out_k_size=17, k_size=7,
                  cls_init_block = ResidualBlock, cls_conv_block = ResidualBlock) -> None:
         super().__init__()
         self.dict_module = nn.ModuleDict()
