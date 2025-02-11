c52301881d055b748e413e6356e322ec0c0b6645
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-11 19:17:16 +0100

    RR

--- code/scripts/roiscriptx05.sh
+++ code/scripts/roiscriptx05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J R9x05        # Nombre del trabajo
-#SBATCH -o R9x05_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e R9x05_error_%j.log    # Archivo para la salida de errores
+#SBATCH -o R3x05_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e R3x05_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=32G                # Memoria total
