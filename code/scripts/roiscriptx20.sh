8d418ee5d70afe7f4bb1eaa53636af152a491642
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-12 12:59:58 +0100

    R3x20

--- code/scripts/roiscriptx20.sh
+++ code/scripts/roiscriptx20.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J R5x2        # Nombre del trabajo
-#SBATCH -o R5x2_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e R5x2_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J R3x20        # Nombre del trabajo
+#SBATCH -o R3x20_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e R3x20_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=120G                # Memoria total
