474918b35cb0f89e4672163f1590a659049dfc90
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-12 12:47:20 +0100

    soft dice

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J Rt7x10        # Nombre del trabajo
-#SBATCH -o Rt7x10_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e Rt7x10_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J Rs7x10        # Nombre del trabajo
+#SBATCH -o Rs7x10_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e Rs7x10_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=32G                # Memoria total
@@ -15,4 +15,4 @@ module load python/3.9.9
 cd /home/usc/ec/rsm/fivesegmentor/
 source ../vroi/bin/activate
 # Ejecutar el script de entrenamiento
-srun python ./code/training/run_benchmark.py -model "RoiNet_FocalTversky"
+srun python ./code/training/run_benchmark.py -model "RoiNet_soft_dice_cldice"
