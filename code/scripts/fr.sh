acd06d4a20b131f4deb07dfe7ef3910953d02b6a
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-12 12:38:04 +0100

    Update fr.sh

--- code/scripts/fr.sh
+++ code/scripts/fr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J Ft5-648        # Nombre del trabajo
-#SBATCH -o Ft5-648_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e Ft5-648_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J Ft5-632        # Nombre del trabajo
+#SBATCH -o Ft5-632_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e Ft5-632_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=32G                # Memoria total
