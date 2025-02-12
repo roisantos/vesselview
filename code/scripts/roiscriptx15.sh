fe121d6e0d09317721b7fb03a5e4522af41e530f
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-12 11:11:45 +0100

    Focal loss

--- code/scripts/roiscriptx15.sh
+++ code/scripts/roiscriptx15.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J R5x15        # Nombre del trabajo
-#SBATCH -o R5x15_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e R5x15_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J Rt5x15        # Nombre del trabajo
+#SBATCH -o Rt5x15_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e Rt5x15_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=64G                # Memoria total
