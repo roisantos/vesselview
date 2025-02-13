560c2bd7b0b33ec92e79e18c8a7003fc3d0cfa85
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-13 11:22:15 +0100

    R1133x10

--- code/scripts/roiscript.sh
+++ code/scripts/roiscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J Rt9x10        # Nombre del trabajo
-#SBATCH -o Rt9x10_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
-#SBATCH -e Rt9x10_error_%j.log    # Archivo para la salida de errores
+#SBATCH -J R1133x10        # Nombre del trabajo
+#SBATCH -o R1133x10_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e R1133x10_error_%j.log    # Archivo para la salida de errores
 #SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
 #SBATCH -c 32                    # 32 núcleos de CPU
 #SBATCH --mem=32G                # Memoria total
@@ -11,8 +11,7 @@
 module load cesga/2020
 module load python/3.9.9
 
-
 cd /home/usc/ec/rsm/fivesegmentor/
 source ../vroi/bin/activate
 # Ejecutar el script de entrenamiento
-srun python ./code/training/run_benchmark.py -model "RoiNet_FocalTversky"
+srun python ./code/training/run_benchmark.py -model "RoiNet_Dice"
