0ecf16b12074ea9ea2925b3700f5fa46bdc780a0
roisantos <62201385+roisantos@users.noreply.github.com>
2025-02-16 10:59:54 +0100

    now in STORE

---
+++ code/scripts/r3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J R3x10        # Nombre del trabajo
+#SBATCH -o R3x10_output_%j.log   # Archivo para la salida estándar (%j expande al JobID)
+#SBATCH -e R3x10_error_%j.log    # Archivo para la salida de errores
+#SBATCH --gres=gpu:a100:1        # Solicita 4 GPU A100
+#SBATCH -c 32                    # 32 núcleos de CPU
+#SBATCH --mem=32G                # Memoria total
+#SBATCH -p medium
+#SBATCH -t 3-00:00:00              # Tiempo máximo de ejecución (2 horas)
+# Cargar módulos necesarios
+module load cesga/2020
+module load python/3.9.9
+
+cd /home/usc/ec/rsm/fivesegmentor/
+source ../vroi/bin/activate
+# Ejecutar el script de entrenamiento
+srun python ./code/training/run_benchmark.py -model "RoiNet3_Dice"
